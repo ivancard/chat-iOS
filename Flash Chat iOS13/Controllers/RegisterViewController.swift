4dda8a47c46a3fb8c0a433fbeca05fd86321afea
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2023-03-02 05:00:53 -0300

    register with firebase implemented

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-    
+    @IBOutlet weak var infoLabel: UILabel!
+
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text,
+           let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    self.infoLabel.text = error.localizedDescription
+                    self.animate()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        self.infoLabel.text = ""
+                        self.animate()
+                    }
+
+                } else {
+                    //Navigate to chatVC
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
-    
+
+    func animate(){
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
 }
